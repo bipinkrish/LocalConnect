d9ccc8eaed45d6b65f4a584076007ebadf3dbac4
Brijesh Krishna <63464137+Brijeshkrishna@users.noreply.github.com>
2023-09-19 18:59:33 +0530

    AppImage support (#2)

---
+++ AppImage/build.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+
+
+# copy the file and folders
+cp build/linux/x64/release/bundle/* .
+
+# detect machine's architecture
+export ARCH=$(uname -m)
+
+# get the missing tools if necessary
+if [ ! -d ../build ]; then mkdir ../build; fi
+if [ ! -x ../build/appimagetool-$ARCH.AppImage ]; then
+  curl -L -o ../build/appimagetool-$ARCH.AppImage https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-$ARCH.AppImage
+  chmod a+x ../build/appimagetool-$ARCH.AppImage 
+fi
+# the build command itself:
+../build/appimagetool-$ARCH.AppImage .
+
+mv *.AppImage LocalConnect.AppImage
